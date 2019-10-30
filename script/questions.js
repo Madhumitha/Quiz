0d5b21522daa0612adb09a74510413c79ffe4dc9
Madhumitha Prabakaran <madhumithabiw@gmail.com>
2019-10-30 16:51:59 -0500

    Basic Questions

---
+++ script/questions.js
@@ -0,0 +1,39 @@
+
+let questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which of the following type of variable takes precedence over other if names are same?",
+        choices: ["global variable", "local variable", "Both of the above", "None of the above"],
+        answer: "local variable"    
+    },
+    {
+        title: "Which built-in method sorts the elements of an array?",
+        choices: ["changeOrder(order)", "order()", "sort()", "None of the above"],
+        answer: "sort()"
+    },
+    {
+        title: "Which of the following function of Number object returns a string value version of the current number?",
+        choices: ["toString()", "toFixed()", "toLocaleString()", "toPrecision()"],
+        answer: "toString()"   
+    }
+]
+
+const length = questions.length;
+console.log(length);
+
+function Question() {
+for(let i=0; i < questions.length; i++) {
+    
+
+
+}
+}
